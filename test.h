9e8d162f12fefcfb89d064494d365e52eea7d526
appsquad <mdishtiyak@appsquad.in>
2016-08-02 17:18:23 +0530

    Commit

---
+++ test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  New Eaze Lyfe
+//
+//  Created by Appsquad Pvt. Ltd. on 02/08/16.
+//  Copyright © 2016 Appsquad Pvt. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol test <NSObject>
+
+@end
